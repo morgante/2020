e77079c58c702ecc86318e6a2aad2997243b6193
Morgante Pell <morgantep@google.com>
2021-01-18 18:38:36 -0500

    Start scrolling...

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import './App.css';
-import { Canvas } from 'react-three-fiber';
+import React, { Suspense, useRef, useState, useEffect } from 'react';
+import { Canvas, Dom } from 'react-three-fiber';
 import { Perf } from 'r3f-perf';
 import Box from './components/Box';
 import { OrbitControls } from '@react-three/drei';
 
 function App() {
+  const [scrollTop, setScrollTop] = useState(0);
+  const [scrollPercent, setScrollPercent] = useState(0);
 
+  const scrollArea = useRef();
+  const onScroll = e => {
+    setScrollTop(e.target.scrollTop);
+    const percent = e.target.scrollTop / (e.target.scrollHeight - e.target.clientHeight);
+    setScrollPercent(percent);
+  };
+  useEffect(() => void onScroll({ target: scrollArea.current }), []);
+
+  const pages = 5;
+
+  console.log("scrollTop", scrollTop, scrollPercent);
 
   return (
     <>
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <OrbitControls />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <OrbitControls enableZoom={false} />
+      <Box position={[-1.2, 0, 0]} percent={scrollPercent} />
       <Perf />
     </Canvas>
+    <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+      <div style={{ height: `${pages * 100}vh` }} />
+    </div>
     </>
   );
 }
--- src/components/Box.js
+++ src/components/Box.js
@@ -2,28 +2,39 @@ import { useRef, useState } from "react"
 import { useFrame } from "react-three-fiber"
 
 export default function Box(props) {
-    // This reference will give us direct access to the mesh
-    const mesh = useRef()
-  
-    // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-  
-    // Rotate mesh every frame, this is outside of React without overhead
-    useFrame(() => {
-      mesh.current.rotation.x = mesh.current.rotation.y += 0.01
-    })
-  
-    return (
-      <mesh
-        {...props}
-        ref={mesh}
-        scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-        onClick={(event) => setActive(!active)}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}>
-        <boxBufferGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-      </mesh>
-    )
+  // This reference will give us direct access to the mesh
+  const mesh = useRef();
+  const size = 1 + (1 * props.percent);
+
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  // const [size, setSize] = useState(1);
+  // const makeBigger = () => {
+  //   setSize(size + 0.5);
+  //   console.log('make bigger');
+  // };
+
+  // console.log('size', size);
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => {
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+  });
+
+  const onClick = (evt) => {
+    console.log("click");
   }
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={[size, size, size]}
+      onClick={onClick}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'red' : 'pink'} />
+    </mesh>
+  )
+}
